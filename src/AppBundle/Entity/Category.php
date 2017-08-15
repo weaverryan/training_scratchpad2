5e0bae0165de188c31616b2988ee925f7326bdd9
Ryan Weaver <ryan@thatsquality.com>
2017-08-14 20:11:03 -0400

    Adding some entities just to generate the DB structure for me

---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="category")
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+}
