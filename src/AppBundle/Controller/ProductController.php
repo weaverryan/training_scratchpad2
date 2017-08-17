18c6aeb7705a0f9f221dcc5833957954e91ccc4c
Ryan Weaver <ryan@thatsquality.com>
2017-08-17 15:48:53 -0500

    Step: Product: Add product HTML list page

---
+++ src/AppBundle/Controller/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProductController extends Controller
+{
+    /**
+     * @Route("/products", name="product_list")
+     */
+    public function indexAction()
+    {
+        $pdo = new \PDO('mysql:host=localhost;dbname=chicago_training', 'root');
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+        $results = $pdo->query('SELECT * FROM product');
+
+        return $this->render('product/index.html.twig', [
+            'products' => $results
+        ]);
+    }
+}
