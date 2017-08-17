671c721c003156311dccad29d0901bb893acc710
Ryan Weaver <ryan@thatsquality.com>
2017-08-17 15:49:02 -0500

    Step: Test: Very basic test

---
+++ tests/AppBundle/Controller/ProductApiControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class ProductApiControllerTest extends TestCase
+{
+    public function testPOSTCreateProduct()
+    {
+        $this->assertEquals(42, 21*2);
+    }
+}
