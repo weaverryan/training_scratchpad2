ee98ce93e8136b6325b0dab407b4037181b9c31f
Ryan Weaver <ryan@thatsquality.com>
2017-08-17 15:49:09 -0500

    Step: Encore: use import/export

--- assets/js/lib/uppercase_element.js
+++ assets/js/lib/uppercase_element.js
@@ -1,3 +1,3 @@
-module.exports = function($el) {
+export default function($el) {
     $el.addClass('text-uppercase');
 };
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-const $ = require('jquery');
-const upperCaseElement = require('./lib/uppercase_element');
-require('../css/global.scss');
+import $ from 'jquery';
+import upperCaseElement from './lib/uppercase_element';
+import '../css/global.scss';
 
 $(document).ready(function() {
     $('h1').on('click', function(e) {
